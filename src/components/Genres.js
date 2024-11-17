228e5e9e3d3f78d4f8d31a9c46447757ec9eb60e
WryaV <48864373+WryaV@users.noreply.github.com>
2024-11-17 21:36:31 +0100

    Adding Login page and Genres Page and Styling them in index.css

---
+++ src/components/Genres.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Genres = () => {
+  return (
+    <div className="genres-container">
+      <p>Here we put genres</p>
+    </div>
+  );
+};
+
+export default Genres;
